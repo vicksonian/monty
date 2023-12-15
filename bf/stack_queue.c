fda65c5eb3a4d43828c97052d5c5a329a4d12b8f
George Michael Pussl <george.pussl@gmail.com>
2023-12-15 14:36:39 -0800

    monty

---
+++ bf/stack_queue.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+#include <stdio.h>
+
+/**
+* stack - Sets the format of the data to a stack (LIFO).
+* @stack: A pointer to the top of the stack.
+* @line_number: The current line number in the Monty bytecode file.
+*/
+void stack(stack_t **stack, unsigned int line_number)
+{
+(void)line_number;
+(void)stack;
+/* No action needed, the default behavior is LIFO (stack) */
+}
+
+/**
+* queue - Sets the format of the data to a queue (FIFO).
+* @stack: A pointer to the top of the stack.
+* @line_number: The current line number in the Monty bytecode file.
+*/
+void queue(stack_t **stack, unsigned int line_number)
+{
+stack_t *temp = *stack;
+(void)line_number;
+
+if (*stack == NULL)
+return;
+while (temp->next != NULL)
+temp = temp->next;
+
+temp->next = *stack;
+(*stack)->prev = temp;
+*stack = (*stack)->next;
+(*stack)->prev = NULL;
+temp->next->next = NULL;
+}

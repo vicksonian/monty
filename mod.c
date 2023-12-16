af65d163cede553e072a2e0c06617971d35ad9b5
CharityZawadi <zawadicharity8@gmail.com>
2023-12-15 18:41:54 -0800

    Betty styling

--- mod.c
+++ mod.c
@@ -2,10 +2,18 @@
 #include <stdio.h>
 
 /**
-* mod_op - Computes the rest of the division of the second
+* f_mod - Computes the rest of the division of the second
 * top element by the top element.
 * @stack: A pointer to the top of the stack.
 * @line_number: The current line number in the Monty bytecode file.
+*
+*  * Description:
+ * - Computes the remainder of dividing the second top
+ *   element by the top element.
+ * - Removes the top element of the stack.
+ * - Exits with EXIT_FAILURE and prints an error message
+ *   if the stack is too short
+ *   or if the top element is zero.
 */
 void f_mod(stack_t **stack, unsigned int line_number)
 {

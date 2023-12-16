a9627ac5246c54ec9f232a55093e543ff0cca66c
CharityZawadi <zawadicharity8@gmail.com>
2023-12-15 18:04:19 -0800

    monty pall.c

--- pall.c
+++ pall.c
@@ -2,21 +2,22 @@
 
 /**
 * f_pall - Prints all the elements of the stack.
-* @head: Pointer to the head of the stack.
+* @head: Stack head.
 * @counter: Unused parameter.
 */
-
 void f_pall(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
+	stack_t *current;
 	(void)counter;
 
-	h = *head;
-	if (h == NULL)
+	current = *head;
+
+	if (current == NULL)
 		return;
-	while (h)
+
+	while (current)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", current->n);
+		current = current->next;
 	}
 }

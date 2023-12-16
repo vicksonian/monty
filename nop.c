8ca3c96f7c9012ee50960ef8759d66bd549620bd
CharityZawadi <zawadicharity8@gmail.com>
2023-12-15 18:45:08 -0800

    betty styling

--- nop.c
+++ nop.c
@@ -1,10 +1,17 @@
 #include "monty.h"
 
 /**
-* nop - Does nothing.
-* @head: Stack head.
-* @stack: A pointer to the top of the stack.
-*/
+ * f_nop - Does nothing.
+ * @head: Stack head (unused).
+ * @counter: The current line number in the Monty bytecode
+ * file (unused).
+ *
+ * Description:
+ * - The nop opcode does nothing and is used as a placeholder.
+ * - It takes no action on the stack or the program execution.
+ * - Parameters (head and counter) are included for
+ *   consistency with other opcode functions.
+ */
 
 void f_nop(stack_t **head, unsigned int counter)
 {

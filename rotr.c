757d06b263b253ec76f11ed6e9b152a10e47f2f1
CharityZawadi <zawadicharity8@gmail.com>
2023-12-15 18:52:18 -0800

    betty style

--- rotr.c
+++ rotr.c
@@ -1,10 +1,20 @@
 #include "monty.h"
 
 /**
-* rotr - Rotates the stack to the bottom.
-* @stack: A pointer to the top of the stack.
-* @line_number: The current line number in the Monty bytecode file.
-*/
+ * f_rotr - Rotates the stack to the bottom.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The current line number in the Monty bytecode file.
+ *
+ * Description:
+ * - The f_rotr opcode rotates the stack to the bottom by moving the top
+ *   element
+ *   to the bottom of the stack.
+ * - Parameters:
+ *    - stack: A pointer to the top of the stack.
+ *      It represents the stack that is rotated.
+ *    - line_number: The current line number in the Monty bytecode file.
+ *      It is used for error reporting and tracking the program's progress.
+ */
 
 void f_rotr(stack_t **stack, unsigned int line_number)
 {
